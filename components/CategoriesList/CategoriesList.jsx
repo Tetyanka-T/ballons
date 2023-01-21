a0f1d23b42907f5987352641da692df2154ccef2
Tatyana Tupalo <tatyana080489t@gmail.com>
2023-01-21 19:51:26 +0200

    fix categories list styles

--- components/CategoriesList/CategoriesList.jsx
+++ components/CategoriesList/CategoriesList.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useState } from "react";
-import { Cansel, Menu } from "../svg";
+import { Cansel, Menu, Select } from "../svg";
 import s from "./CategoriesList.module.scss";
 
 const CategoriesList = () => {
@@ -12,74 +12,99 @@ const CategoriesList = () => {
     SetShowMenu(true);
     toggleMenu();
   };
-
   const toggleMenu = () => {
     showMenu ? SetShowMenu(false) : SetShowMenu(true);
   };
 
+  const onShowBaby = () => {
+    SetBaby(true);
+    toggleBaby();
+  };
+  const toggleBaby = () => {
+    baby ? SetBaby(false) : SetBaby(true);
+  };
+
+  const onShowBirthDays = () => {
+    SetBaby(true);
+    toggleBirthDays();
+  };
+  const toggleBirthDays = () => {
+    birthDay ? SetBirthDay(false) : SetBirthDay(true);
+  };
+
   return (
     <div>
-      <button onClick={() => onShowMenu()}>
+      <button onClick={() => onShowMenu()} className={s.showMenu}>
         {showMenu === false && <Menu />}
         {showMenu === true && <Cansel />}
       </button>
       {showMenu && (
         <ul className={s.categories_list}>
-          <li>
+          <li className={s.categories_list__item}>
+            <Link href="/categories">Всі композиції з кульок</Link>
+          </li>
+          <li className={s.categories_list__item}>
             <Link href="/categories/gender-party">Gender party</Link>
           </li>
-          <li>
-            <button onClick={() => SetBaby(true)}>
+          <li className={s.categories_list__item}>
+            <button onClick={() => onShowBaby()} className={s.button_select}>
               Виписка з пологового будинку
+              <Select className={s.button_baby__icon} />
             </button>
             {baby && (
-              <ul>
-                <li>
+              <ul className={s.categories_listBaby}>
+                <li className={s.categories_listBaby__item}>
                   <Link href="/categories/baby">Всі</Link>
                 </li>
-                <li>
-                  <Link href="/categories/baby/boy">Хлопчик</Link>
+                <li className={s.categories_listBaby__item}>
+                  <Link href="/categories/baby/girl">Для дівчинки</Link>
                 </li>
-                <li>
-                  <Link href="/categories/baby/girl">Дівчинка</Link>
+                <li className={s.categories_listBaby__item}>
+                  <Link href="/categories/baby/boy">Для хлопчика</Link>
                 </li>
               </ul>
             )}
           </li>
-          <li>
-            <button onClick={() => SetBirthDay(true)}>День народження</button>
+          <li className={s.categories_list__item}>
+            <button
+              onClick={() => onShowBirthDays()}
+              className={s.button_select}
+            >
+              День народження
+              <Select className={s.button_birthDay__icon} />
+            </button>
             {birthDay && (
-              <ul>
-                <li>
+              <ul className={s.categories_listBirtDay}>
+                <li className={s.categories_listBirtDay__item}>
                   <Link href="/categories/birthday">Всі</Link>
                 </li>
-                <li>
+                <li className={s.categories_listBirtDay__item}>
                   <Link href="/categories/birthday/first">
-                    перший день народження
+                    Перший день народження
                   </Link>
                 </li>
-                <li>
-                  <Link href="/categories/birthday/girls">для дівчинки</Link>
+                <li className={s.categories_listBirtDay__item}>
+                  <Link href="/categories/birthday/girls">Для дівчинки</Link>
                 </li>
-                <li>
-                  <Link href="/categories/birthday/boys">для хлопчика</Link>
+                <li className={s.categories_listBirtDay__item}>
+                  <Link href="/categories/birthday/boys">Для хлопчика</Link>
                 </li>
-                <li>
-                  <Link href="/categories/birthday/she">для неї</Link>
+                <li className={s.categories_listBirtDay__item}>
+                  <Link href="/categories/birthday/she">Для неї</Link>
                 </li>
-                <li>
-                  <Link href="/categories/baby/he">для нього</Link>
+                <li className={s.categories_listBirtDay__item}>
+                  <Link href="/categories/baby/he">Для нього</Link>
                 </li>
               </ul>
             )}
           </li>
-          <li>
+          <li className={s.categories_list__item}>
             <Link href="/categories/photo-zone">Фотозони</Link>
           </li>
-          <li>
+          <li className={s.categories_list__item}>
             <Link href="/categories/thematics">Тематичні свята</Link>
           </li>
-          <li>
+          <li className={s.categories_list__item}>
             <Link href="/categories/design">Оформлення фасадів</Link>
           </li>
         </ul>
--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,7 +1,27 @@
-const Categories = () => {
-    return <>
-    <h1>Categories</h1>
-    </>
+import Head from 'next/head'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+const CategoriesPage = () => {
+    return (
+        <div>
+        <Head>
+          <title>Весела витівка</title>
+          <meta name="description" content="Інтернет магазин виробів із повітряних кульок, оформлення свят" />
+          <link rel="icon" href="" />
+        </Head>
+  
+        <main>
+          <Header />
+          <h1>Всі кульки</h1>
+        </main>
+  
+        <footer>
+          <Footer/>
+        </footer>
+      </div>
+    )
 }
 
-export default Categories;
+
+export default CategoriesPage;
