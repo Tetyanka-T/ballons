73fdfbfd0584bf722d820f5477e848964626e4f5
Tatyana Tupalo <tatyana080489t@gmail.com>
2023-02-13 20:01:11 +0200

    add orderPage

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
+import Image from "next/image";
 import Header from "../components/Header/Header";
 import OrderForm from "../components/OrderForm/OrderForm";
 import Footer from "../components/Footer/Footer";
 import common from "../styles/common.module.scss";
 import s from "../styles/Categories.module.scss";
-import { Home, NextPage } from "../components/svg";
+import ss from "../styles/OrderPage.module.scss";
+import { Home, NextPage, Cross } from "../components/svg";
+
 
 const OrderPage = () => {
   return (
@@ -32,6 +35,36 @@ const OrderPage = () => {
           </ul>
           <h1 className={common.section_title}>Оформлення замовлення</h1>
           <OrderForm/>
+          <h2 className={ss.order}>Ваше замовлення</h2>
+          <ul className={ss.basket_list}>
+          <li className={ss.basket_list__item}>
+          <div className={ss.basket_photo}>
+            <Image src="/photo_1.jpg" alt="" width={70} height={90} />
+          </div>
+
+          <div className={ss.basket_list__item__product}>
+            <h3 className={ss.basket_list__item__product__title}>
+              Супер пупер композиція
+            </h3>
+            <p className={ss.basket_list__item__product__art}>арт.№ 1236</p>
+            <p className={ss.basket__price}>Ціна: 2000 грн</p>
+          </div>
+          </li>
+          <li className={ss.basket_list__item}>
+          <div className={ss.basket_photo}>
+            <Image src="/photo_1.jpg" alt="" width={70} height={90} />
+          </div>
+
+          <div className={ss.basket_list__item__product}>
+            <h3 className={ss.basket_list__item__product__title}>
+              Супер пупер композиція
+            </h3>
+            <p className={ss.basket_list__item__product__art}>арт.№ 1236</p>
+            <p className={ss.basket__price}>Ціна: 2000 грн</p>
+          </div>
+          </li>
+          </ul>
+        
         </div>
       </main>
 
