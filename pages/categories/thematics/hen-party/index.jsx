b5ec2a2b65d3eaa9634eb99288da57c86d30c54a
Tatyana Tupalo <tatyana080489t@gmail.com>
2023-07-01 14:52:56 +0300

    comment thematics pages

--- pages/categories/thematics/hen-party/index.jsx
+++ pages/categories/thematics/hen-party/index.jsx
@@ -1,33 +1,31 @@
-import Head from 'next/head'
-import common from '../../../../styles/common.module.scss'
-import BalloonCard from '../../../../components/BalloonCard/BalloonCard'
+// import Head from 'next/head'
+// import common from '../../../../styles/common.module.scss'
+// import BalloonCard from '../../../../components/BalloonCard/BalloonCard'
 
-import NoFindComposition from '../../../../components/NoFindComposition/NoFindComposition'
-import Novigation from '../../../../components/Navigation/Novigation'
+// import NoFindComposition from '../../../../components/NoFindComposition/NoFindComposition'
+// import Novigation from '../../../../components/Navigation/Novigation'
 
-const HenParty = ({balloon}) => {
-    return (
-        <div>
-        <Head>
-          <title>Весела витівка</title>
-          <meta name="description" content="Інтернет магазин виробів із повітряних кульок, оформлення свят" />
-          <link rel="icon" href="/balloon.svg" />
-        </Head>
-  
-        <main className={common.container}>
-          
-            <Novigation section='Тематична свята' category='Дівич-вечір' link='/categories/thematics'/>
-            <h1 className={common.section_title}>Дівич-вечір</h1>
-         
-            <BalloonCard balloon={balloon}/>
-        
-        </main>
-        <NoFindComposition/>
-  
-      
-      </div>
-    )
-}
+// const HenParty = ({balloon}) => {
+//     return (
+//         <div>
+//         <Head>
+//           <title>Весела витівка</title>
+//           <meta name="description" content="Інтернет магазин виробів із повітряних кульок, оформлення свят" />
+//           <link rel="icon" href="/balloon.svg" />
+//         </Head>
 
+//         <main className={common.container}>
 
-export default HenParty;
+//             <Novigation section='Тематична свята' category='Дівич-вечір' link='/categories/thematics'/>
+//             <h1 className={common.section_title}>Дівич-вечір</h1>
+
+//             <BalloonCard balloon={balloon}/>
+
+//         </main>
+//         <NoFindComposition/>
+
+//       </div>
+//     )
+// }
+
+// export default HenParty;
